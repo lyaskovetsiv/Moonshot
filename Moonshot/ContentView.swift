2a4312df34b72ec3575ca3918dc46b9a87b22b77
Иван Лясковец <lyaskovets.iv@MacBook-Air-Ivan.local>
2023-08-05 17:42:27 +0300

    UI changes in ContentView

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -29,19 +29,32 @@ struct ContentView: View {
 									.resizable()
 									.scaledToFit()
 									.frame(width: 100, height: 100)
+									.padding()
 								VStack {
 									Text(mission.displayName)
 										.font(.headline)
+										.foregroundColor(.white)
 									Text(mission.formattedLaunchDate)
 										.font(.caption)
+										.foregroundColor(.white.opacity(0.5))
 								}
+								.padding(.vertical)
 								.frame(maxWidth: .infinity)
+								.background(.lightBackgroundColor)
+							}
+							.clipShape(RoundedRectangle(cornerRadius: 10))
+							.overlay {
+								RoundedRectangle(cornerRadius: 10)
+									.stroke(.lightBackgroundColor)
 							}
 						}
 					}
 				}
+				.padding([.horizontal, .bottom])
 			}
 			.navigationTitle("Moonshot")
+			.background(.darkBackgroundColor)
+			.preferredColorScheme(.dark)
 		}
     }
 }
